c4322b24f2f4e15098bf426504465012c8f08733
ayou2021 <76209934+ayou2021@users.noreply.github.com>
2021-12-17 09:50:23 +0700

    接口调整

--- app/Http/Controllers/Api/CommHomeController.php
+++ app/Http/Controllers/Api/CommHomeController.php
@@ -46,7 +46,7 @@ class CommHomeController extends Controller
         $perPage = 6;
         $paginator = CommBbs::query()
             ->leftJoin('users', 'community_bbs.author_id', '=', 'users.id')
-            ->select('community_bbs.id', 'content', 'thumbs', 'likes', 'comments', 'rewards', 'users.location_name', 'community_bbs.updated_at', 'nickname', 'sex', 'is_office', 'video', 'users.id as uid', 'users.avatar', 'users.level', 'users.vip as vipLevel')
+            ->select('community_bbs.id', 'content', 'thumbs', 'likes', 'comments', 'rewards', 'users.location_name', 'community_bbs.updated_at', 'nickname', 'sex', 'is_office', 'video', 'users.id as uid', 'users.avatar', 'users.level', 'users.vip as vipLevel','video_picture')
             ->where('community_bbs.author_id', $id)->orderBy('updated_at', 'desc')
             ->orderBy('updated_at')
             ->simplePaginate($perPage, ['*'], '', $page);
