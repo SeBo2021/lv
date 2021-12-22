279f0d9889151a3e8e38af1f6dbd5a605c2cd3ff
ayou2021 <76209934+ayou2021@users.noreply.github.com>
2021-12-22 19:50:49 +0700

    代码调整

--- resources/views/admin/default/tpl/form/movie.blade.php
+++ resources/views/admin/default/tpl/form/movie.blade.php
@@ -8,9 +8,9 @@
     @endphp
     @include('admin.default.tpl.form.text',['form_item'=>$form_item])
     <div class="mb-10">
-{{--        <iframe id="videoIframe" width="345" height="200" src="/player/dash.html" frameborder="0" allowfullscreen></iframe>--}}
+        {{--        <iframe id="videoIframe" width="345" height="200" src="/player/dash.html" frameborder="0" allowfullscreen></iframe>--}}
         <video width="345" height="200" id="dashjs" controls></video>
-{{--        <button class="layui-btn layui-btn-white layui-btn-sm iupload-area-img-show-btn {{ $form_item['value']?'':'none' }}" type="button">删除</button>--}}
+        {{--        <button class="layui-btn layui-btn-white layui-btn-sm iupload-area-img-show-btn {{ $form_item['value']?'':'none' }}" type="button">删除</button>--}}
     </div>
     <div class="controls">
         <label class="layui-btn layui-btn-sm">
@@ -24,24 +24,24 @@
         <input type="hidden" name="{{ $form_item['field'] }}" value="{{ $form_item['value'] }}" id="aetherupload-savedpath"><!--需要一个自定义名称的id，以及一个自定义名称的name值, 用以标识资源储存路径自动填充位置，默认id为aetherupload-savedpath，可根据setSavedPathField(...)设置为其它任意值-->
         <input type="hidden" id="callback_upload" name="callback_upload" value="0">
     </div>
-    {{ storage_host_field() }} <!--（可选）需要标识资源服务器host地址的field，用以支持分布式部署-->
-    {{ csrf_field() }} <!--需要标识csrf token的field-->
+{{ storage_host_field() }} <!--（可选）需要标识资源服务器host地址的field，用以支持分布式部署-->
+{{ csrf_field() }} <!--需要标识csrf token的field-->
     <div id="result"></div>
 </div>
 <script src="{{ \Illuminate\Support\Facades\URL::asset('vendor/aetherupload/js/aetherupload-all.js') }}"></script><!--需引入aetherupload-core.js、zepto.min.js（类似jquery，更轻量化，可与jquery互相代替）、spark-md5.min.js，此文件已包含上述全部，也可分别单独引入 -->
 
 @if(!empty($form_item['value']))
-<script>
-    var real_use_url = "{{ \App\Jobs\VideoSlice::get_slice_url($form_item['value'],'dash',$form_item['sync']) }}";
-    console.log(real_use_url);
-    // $("#videoIframe").attr('src',real_use_url);
-    //=====================以上打印出同步资源地址======================
-    var url = "{{ \App\Jobs\VideoSlice::get_slice_url($form_item['value']) }}";
-    var playerElement = $('#dashjs');
-    playerElement.show();
-    var player = dashjs.MediaPlayer().create();
-    player.initialize(document.querySelector('#dashjs'), real_use_url, false); //true 为自动播放
-</script>
+    <script>
+        var real_use_url = "{{ \App\Jobs\VideoSlice::getOrigin($form_item['sync'],$form_item['url'])}}";
+        console.log(real_use_url);
+        // $("#videoIframe").attr('src',real_use_url);
+        //=====================以上打印出同步资源地址======================
+        var url = "{{ \App\Jobs\VideoSlice::get_slice_url($form_item['value']) }}";
+        var playerElement = $('#dashjs');
+        playerElement.show();
+        var player = dashjs.MediaPlayer().create();
+        player.initialize(document.querySelector('#dashjs'), real_use_url, false); //true 为自动播放
+    </script>
 @endif
 <script>
     // success(someCallback)中声名的回调方法需在此定义，参数someCallback可为任意名称，此方法将会在上传完成后被调用
