5e2d2cefe284b6d4b51d5fabc65489e9eaf09550
zhaosi <zhaosi@git.com>
2022-01-12 15:21:11 +0800

    1

--- app/Http/Controllers/Api/CommMessageController.php
+++ app/Http/Controllers/Api/CommMessageController.php
@@ -76,7 +76,7 @@ class CommMessageController extends Controller
 
                     $unReadUserKey = 'status_me_unread_' . $uid;
                     $unReads = $this->redis()->sMembers($unReadUserKey);
-                    $items[$k]['no_read'] = !in_array($toUserId,$unReads) ? 0 : 1;
+                    $items[$k]['no_read'] = in_array($toUserId,$unReads) ? 0 : 1;
                     //Log::info('===CommMessage==',[$unReadUserKey,$unReads,$toUserId,!in_array($toUserId,$unReads),$items[$k]['no_read']]);
                 }
             }
