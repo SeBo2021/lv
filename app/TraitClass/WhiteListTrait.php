52a6346656585ed8ed54aed84f07e05bef5c23e0
zhaosi <zhaosi@git.com>
2022-01-13 20:02:05 +0800

    1

--- app/TraitClass/WhiteListTrait.php
+++ app/TraitClass/WhiteListTrait.php
@@ -4,6 +4,7 @@ namespace App\TraitClass;
 
 use App\Models\WhiteList;
 use App\TraitClass\IpTrait;
+use Illuminate\Support\Facades\Log;
 
 trait WhiteListTrait
 {
@@ -15,6 +16,7 @@ trait WhiteListTrait
             ->where('status',1)
             ->where('type',1)
             ->pluck('ip')->toArray();
+        Log::info('===adminLoginIPS===',[$whiteList,$ip]);
         if(!in_array($ip, $whiteList)){
             return false;
         }
