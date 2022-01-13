ea8d92fba9787e6b61a77de22dc63e1dcb65219c
zhaosi <zhaosi@git.com>
2022-01-13 19:58:20 +0800

    1

--- app/Http/Controllers/Api/VideoShortController.php
+++ app/Http/Controllers/Api/VideoShortController.php
@@ -166,7 +166,7 @@ class VideoShortController extends Controller
                 $starId = '0';
             }
             $res = $this->items($page, $user, $starId,$cateId,$tagId,$words);
-            Log::info('===VideoShortLists===',[$params]);
+            //Log::info('===VideoShortLists===',[$params]);
             return response()->json([
                 'state' => 0,
                 'data' => $res
