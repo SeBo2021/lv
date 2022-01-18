c087bf27e900e946dd5932197b61a1879ff88ea7
zhaosi <zhaosi@git.com>
2022-01-18 18:32:00 +0800

    1

--- app/Http/Controllers/Admin/ShortController.php
+++ app/Http/Controllers/Admin/ShortController.php
@@ -507,7 +507,7 @@ class ShortController extends BaseCurlController
             switch ($field){
                 case 'slice':
                     $items = VideoShort::query()->whereIn($id, $id_arr)->get(['id','url','hls_url','dash_url']);
-                    $domain = env('SLICE_DOMAIN');
+                    $domain = env('RESOURCE_DOMAIN');
                     foreach ($items as $item){
                         if(empty($item->hls_url) || empty($item->dash_url)){
                             $this->saveOriginFile($domain . $item->url);
