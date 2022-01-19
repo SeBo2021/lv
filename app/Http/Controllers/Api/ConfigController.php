923632da8469a8daa884da334bbe82cfb112ceea
zhaosi <zhaosi@git.com>
2022-01-19 19:19:07 +0800

    公1

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -16,7 +16,7 @@ class ConfigController extends Controller
     {
         $appConfig = config_cache('app');
         if(!empty($appConfig)){
-            $res['announcement'] = $appConfig['announcement'];
+            $res['announcement'] = stripslashes($appConfig['announcement']);
             $res['anActionType'] = $appConfig['announcement_action_type'];
             //视频ID
             $res['videoId'] = $appConfig['announcement_video_id'];
