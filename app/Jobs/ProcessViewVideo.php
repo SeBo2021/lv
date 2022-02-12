1840cd1b7ff0bead0485412c0e6938a749d2ed0b
zhaosi <zhaosi@git.com>
2022-02-12 13:17:55 +0800

    1

--- app/Jobs/ProcessViewVideo.php
+++ app/Jobs/ProcessViewVideo.php
@@ -45,12 +45,14 @@ class ProcessViewVideo implements ShouldQueue
         //插入历史记录
         DB::table('view_history')->insertOrIgnore(['uid'=>$uid,'vid'=>$vid]);
 
-        /*if($this->video['restricted']==2){
-            if($this->userModel->gold >= ($this->video['gold'])){
-                User::query()->where('id',$uid)->decrement('gold',$this->video['gold']);
-            }
-        }*/
         if($this->userModel->long_vedio_times>0){
+            //统计激活
+            $configData = config_cache('app');
+            $setTimes = $configData['free_view_long_video_times'] ?? 0;
+            if(($this->userModel->long_vedio_times==$setTimes) && (date('Y-m-d')==date('Y-m-d',strtotime($this->userModel->created_at)))){
+                $this->saveStatisticByDay('active_view_users',$this->userModel->channel_id,$this->userModel->device_system);
+            }
+            //
             User::query()->where('id',$uid)->decrement('long_vedio_times'); //当日观看次数减一
         }
         //
