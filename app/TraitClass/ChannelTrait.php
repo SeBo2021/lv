db6f1fd790536b2a1c2d1df2da9dcca5483b446e
zhaosi <zhaosi@git.com>
2022-02-14 16:24:53 +0800

    1

--- app/TraitClass/ChannelTrait.php
+++ app/TraitClass/ChannelTrait.php
@@ -141,7 +141,7 @@ trait ChannelTrait
         }else{
             $query = $query->where('status',1);
         }
-        $channels = $query->get(['id','pid','name','promotion_code','number','number','share_ratio']);
+        $channels = $query->get();
 
         foreach ($channels as $channel) {
             $exists = DB::table($statistic_table)->where('channel_id', $channel->id)->where('date_at', $currentDate)->exists();
@@ -153,6 +153,7 @@ trait ChannelTrait
                     'channel_promotion_code' => $channel->promotion_code,
                     'channel_code' => $channel->number,
                     'channel_status' => 1,
+                    'unit_price' => $channel->unit_price,
                     'share_ratio' => $channel->share_ratio,
                     'total_recharge_amount' => 0,
                     'total_amount' => 0,
