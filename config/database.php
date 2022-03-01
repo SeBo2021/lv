b0ae7640c12f0f5c21123ac894aad39e700770bf
ayou <ayou@no.no>
2022-03-01 20:40:58 +0800

    加位

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => env('DB_PREFIX',''),
             'prefix_indexes' => true,
-            'strict' =>  env('DB_STRICT', false),
+            'strict' =>  env('DB_STRICT', true),
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
