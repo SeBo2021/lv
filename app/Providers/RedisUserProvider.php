8c0e09fe1fc4f9f2579e77d3cd05165bcc19dc2f
zhaosi <zhaosi@git.com>
2022-03-08 22:37:44 +0800

    1

---
+++ app/Providers/RedisUserProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\TraitClass\PHPRedisTrait;
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Support\Facades\Redis;
+
+class RedisUserProvider extends EloquentUserProvider
+{
+    use PHPRedisTrait;
+
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param  array  $credentials
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByCredentials(array $credentials)
+    {
+        if (!isset($credentials['api_token'])) {
+            return;
+        }
+
+        $userId = Redis::get($credentials['api_token']);
+
+        return $this->retrieveById($userId);
+    }
+
+
+}
