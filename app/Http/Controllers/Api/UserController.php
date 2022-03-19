3bfe31587e8837998ec3f08ac83e49b60ce4e09b
zhaosi <zhaosi@git.com>
2022-03-19 18:32:09 +0800

    1

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -562,6 +562,7 @@ class UserController extends Controller
             Cache::forget("cachedUser.{$user->id}");
             Cache::forget("cachedUser.".$requestUser['id']);
             Token::query()->where('name',$user->account)->delete();
+            Token::query()->where('id',$tokenId)->delete();
             return response()->json(['state'=>0, 'data'=>$requestUser, 'msg'=>'账号找回成功']);
         }
         return response()->json([]);
