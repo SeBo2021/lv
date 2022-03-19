19853f6437f8dbde8224a32155001dc9040aeac0
zhaosi <zhaosi@git.com>
2022-03-19 16:02:27 +0800

    1

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -274,6 +274,11 @@ class MemberController extends BaseCurlController
                 'name' => '会员ID',
             ],
             [
+                'field' => 'query_phone_number',
+                'type' => 'text',
+                'name' => '手机号',
+            ],
+            [
                 'field' => 'query_like_account',//这个搜索写的查询条件在app/TraitClass/QueryWhereTrait.php 里面写
                 'type' => 'text',
                 'name' => '账号',
