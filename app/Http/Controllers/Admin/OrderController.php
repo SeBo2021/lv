64172d4f9a5414517c0e3b8947aefcc758c077f9
ayou <no@no.no>
2022-03-31 17:02:28 +0800

    支付渠道

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -132,7 +132,7 @@ class OrderController extends BaseCurlController
             '1' => '大白鲨支付',
             '101' => '信达支付',
             '102' => '艾希支付',
-            default => '大白鲨支付',
+            default => '--',
         };
         return $item;
     }
