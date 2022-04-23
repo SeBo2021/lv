3bc8badd84c1893a5a3204419a1d525b63859713
zhaosi <zhaosi@git.com>
2022-04-23 18:17:12 +0800

    1

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -124,14 +124,7 @@ class HomeController extends Controller
                 $ids = $redis->sMembers('catForVideo:'.$item['id']);
                 if(!empty($ids)){
                     $queryBuild = Video::search('*')->where('status',1)->whereIn('id',$ids);
-//                    $queryBuild = DB::table('video')->where('status',1)->whereIn('id',$ids);
-                    /*if($item['is_rand']==1){
-                        $queryBuild = $queryBuild->inRandomOrder();
-                    }else{
-                        $queryBuild = $queryBuild->orderByRaw('video.sort DESC,video.updated_at DESC,video.id DESC');
-                    }*/
                     $limit = $item['limit_display_num']>0 ? $item['limit_display_num'] : 8;
-                    //$videoList = $queryBuild->limit($limit)->get($this->videoFields)->toArray();
                     $videoList = $queryBuild->simplePaginate($limit, 'searchPage', 1)->toArray()['data'];
                     if($item['is_rand']==1){
                         shuffle($videoList);
@@ -141,7 +134,6 @@ class HomeController extends Controller
                             'updated_at' => 'desc',
                             'id' => 'desc',
                         ]);
-                        //$queryBuild = $queryBuild->orderByRaw('video.sort DESC,video.updated_at DESC,video.id DESC');
                     }
                     $videoList = $this->handleVideoItems($videoList,false,$request->user()->id);
                     $item['small_video_list'] = $videoList;
