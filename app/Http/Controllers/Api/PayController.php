dd170f1c3bd4125c7dcb2eda7dae6019e7150302
zhaosi <zhaosi@git.com>
2022-04-24 12:36:31 +0800

    1

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\TraitClass\ApiParamsTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class PayController extends PayBaseController
 {
     use ApiParamsTrait;
-    public function entrance(Request $request): \Illuminate\Http\JsonResponse
+    public function entrance(Request $request,$channel): \Illuminate\Http\JsonResponse
     {
-        return response()->json($request->all());
+        $params = ApiParamsTrait::parse($request->params ?? '');
+        Validator::make($params, [
+            'pay_id' => 'required|string',
+            'type' => [
+                'required',
+                'string',
+                Rule::in(['1', '2']),
+            ],
+        ])->validated();
+        dump($channel);
+        return response()->json($params);
     }
 
 }
