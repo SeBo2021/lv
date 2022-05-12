61f6508ff03f28b7bd608d7a9f792a6d3852841e
zhaosi <zhaosi@git.com>
2022-05-12 18:16:16 +0800

    1

--- app/TraitClass/VideoTrait.php
+++ app/TraitClass/VideoTrait.php
@@ -485,7 +485,7 @@ trait VideoTrait
             }
             //hls播放地址处理
             if(isset($list['hls_url'])){
-                $list['hls_url'] = $domainSync . $this->transferHlsUrl($list['hls_url']);
+                $list['hls_url'] = $domainSync . $this->transferHlsUrl($list['hls_url']??'');
             }
             $list['preview_hls_url'] = $domainSync . $list['preview_hls_url'];
             /*$previewHlsInfo = pathinfo($list['preview_hls_url']);
@@ -501,6 +501,7 @@ trait VideoTrait
 
     public function generateRandViews($views): string
     {
+        $views = intval($views);
         $views *= 20;
         $length = strlen($views);
         if($length > 8){
